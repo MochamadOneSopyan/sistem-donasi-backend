c3a9a93f1c2281ebc90316ab25c0622edecabc32
Yans Piyan <111016644+MochamadOneSopyan@users.noreply.github.com>
2026-09-24 00:25:52 +0700

    Update db.js

--- sistem-donasi/src/db.js
+++ sistem-donasi/src/db.js
@@ -1,4 +1,8 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
+prisma.$connect()
+  .then(() => console.log("✅ Database connected"))
+  .catch((err) => console.error("❌ Database connection failed:", err));
+
 module.exports = prisma;
